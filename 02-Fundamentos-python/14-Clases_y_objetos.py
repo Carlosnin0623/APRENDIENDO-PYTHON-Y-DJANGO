9a69ebf4a0a08f84d3d778949ea705ee7789f1ba
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-06-05 22:31:51 -0400

    update clase y objetos

--- 02-Fundamentos-python/14-Clases_y_objetos.py
+++ 02-Fundamentos-python/14-Clases_y_objetos.py
@@ -9,4 +9,21 @@ elemento u objeto.
 """
 
 class Person:
-    pass
+    # Contructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    # Método
+    def work(self):
+        return f"{self.name} esta trabajando duro"
+    
+    
+# Intancia de una clase
+
+persona1 = Person(name='Eduardo',age=28)
+
+persona2 = Person(name='Elizabeth', age=30)
+
+print(persona1.work())
+print(persona2.work())
