0c568cbe3c94b5d8cbfc7566b07f3ca1581b58f5
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-06-18 21:23:21 -0400

    Creandos proyectos con Django

--- 03-Django_Fundamentos/myproject/myproject/settings.py
+++ 03-Django_Fundamentos/myproject/myproject/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# Aqui es donde debemos agregar las apps que creamos
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'alumnos', # Instando la aplicacion alumnos en django
+    'cursos' # Instando la aplicacion cursos en django
 ]
 
 MIDDLEWARE = [
