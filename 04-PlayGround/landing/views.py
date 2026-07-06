d9f5d6e91d63d0b930988f310dab340445f9a49f
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2026-07-05 23:04:06 -0400

    Aprendiendo sobre los tags

--- 04-PlayGround/landing/views.py
+++ 04-PlayGround/landing/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import date
+
 # Create your views here.
 def home(request):
+    today = date.today()
+    stack = ['Python', 'Django','Goolang', 'PHP', 'JS']
     return render(request,'landing/landing.html', {
-        'name': 'Ricardo'
+        'name': 'Ricardo',
+        'age': 28,
+        'today': today,
+        'stack': ''
     })
