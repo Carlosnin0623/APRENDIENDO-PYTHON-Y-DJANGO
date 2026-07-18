ccf9be30c1c0e1b1ac2ac0287da5a9fc48e8057d
Carlos Alberto Nin Queliz <carlosnin648@gmail.com>
2026-07-17 20:55:37 -0400

    Aprendiendo sobre Django

--- 04-PlayGround/quotes/views.py
+++ 04-PlayGround/quotes/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.shortcuts import render
 
@@ -31,13 +31,6 @@ days_of_week = {
     "sunday": "Da un poquito más todos los días"
 }
 
-
-def days_week(request, day):
-    try:
-     quote_text = days_of_week[day]
-     return HttpResponse(quote_text)
-    except Exception:
-     return HttpResponseNotFound("Este día no existe")
     
 def days_week_with_number(request, day):
     days = list(days_of_week.keys())
@@ -50,4 +43,14 @@ def days_week_with_number(request, day):
     redirect_path = reverse("day-quote", args=[redirect_day])
 
     return HttpResponseRedirect(redirect_path)
+
+def days_week(request, day):
+    try:
+     quote_text = days_of_week[day]
+     return HttpResponse(quote_text)
+    except Exception:
+     # return HttpResponseNotFound("Este día no existe")
+     return render(request, "./includes/404.html")
+     raise Http404() # Esto generara el error 404 pero ya en producción y buscara automaticamente el archivo 404.html
+     # Pero por ahora usa el render para hacer pruebas con el archivo
     
